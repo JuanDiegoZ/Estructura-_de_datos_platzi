821d8b3693152d40c0da1e95253388139b419d41
Juan DIego Zipaquira A <juan444j@gmail.com>
2022-06-08 17:01:42 -0500

    dataStructure-stacks

---
+++ stacks.js
@@ -0,0 +1,38 @@
+// stacks.
+//Metodo pop = Remover el ultimo elemento.
+//Metodo push = Agregar un elemento al final.
+//metodo peek = tomar el ulitmo elemento de la linea.
+// El stack basicamente es apilar elementos unos ensima de otros.
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null
+    }
+}
+class stack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0; 
+    }
+    peek(){
+        return this.top;
+
+    }
+    push(value){
+        const newNode =new Node(value);
+        if (this.length === 0)
+        {
+            this.bottom = newNode;
+            this.top = newNode;
+        }
+        else
+        {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer
+        }
+        this.length ++;
+    }   
+
+}
